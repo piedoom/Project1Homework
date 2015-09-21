cc9288b3916f20ee6fbfa7fe2133ddeb48461352
unknown <apl2389@GDD-23.main.ad.rit.edu>
2015-09-21 10:34:26 -0400

    added some more stuff

--- Project1LozadaAlexander/Program.cs
+++ Project1LozadaAlexander/Program.cs
@@ -10,6 +10,64 @@ namespace Project1LozadaAlexander
     {
         static void Main(string[] args)
         {
+            Item couch = new Item("couch", 10000, 20);
+            Console.WriteLine();
         }
     }
+
+    public class Manager
+    {
+        string name;
+        //TODO: pull these from a DB
+        int[] inventory = new int[1000];        
+    }
+
+    //general class for typical items 
+    public class Item
+    {
+        //base class props and fields
+        string name;
+        int priceCents;
+        int stock;
+
+        public string Name { get; set; }
+        public int PriceCents { get; set; }
+        public int Stock { get; set; }
+
+        //constructor
+        public Item(string name, int priceCents, int stock)
+        {
+            this.name = name;
+            this.priceCents = priceCents;
+            this.stock = stock;
+        }
+
+        public string GetInfo()
+        {
+            //get info on product
+            return ("");
+        }
+
+        public void Buy(int amount = 1)
+        {
+            //buy 1 if no value is supplied
+            stock -= amount;
+        }
+
+        public void Add(int amount = 1)
+        {
+            //add 1 if no value is supplied
+            stock += amount;
+        }
+    }
+
+    //create an item that is used only by the store; unsellable
+    /*public class StoreItem : Item
+    {
+        public StoreItem(string name, int priceCents, bool inStock, int stock) : base(name, priceCents, inStock, stock)
+        {
+
+        }
+    }
+    */
 }
